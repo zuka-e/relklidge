3c91ae5b677555a71493e4f9aba6e62ad55ce85f
zuka-e <nq.iizuka@gmail.com>
2019-09-12 06:58:27 +0000

    Add category-index-layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     # sections => (side-bar用)
     @sections = Section.where(category_id: @category.id)
-    # 例.タグに民法が含まれる投稿
+    # 例.タグに"民法"が含まれる投稿
     @posts = Post.joins(:tags).where("tags.name IN (?)",@category.name).page(params[:page])
   end
 end
