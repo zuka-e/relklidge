06681bb0460952d4cc9cef0c18f10e36c852455a
zuka-e <nq.iizuka@gmail.com>
2019-09-14 20:50:07 +0000

    Add update-form for comment & Add destroy func

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,28 @@ class CommentsController < ApplicationController
       redirect_back(fallback_location: params[:stored_url])
     end
   end
+  def update
+    @comment = Comment.find(params[:id])
+    @post = Post.find_by(id: params[:post_id])
+      respond_to do |format|
+				format.html { redirect_back(fallback_location: params[:stored_url]) }
+				format.js
+      end
+    if params[:comment].present?
+      @comment.update(comment_params)
+      respond_to do |format|
+        format.html { redirect_back(fallback_location: params[:stored_url]) }
+        format.js { render 'create' }
+      end
+    end
+  end
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.destroy
+      flash[:danger] = 'コメントは削除されました'
+      redirect_back(fallback_location: params[:stored_url])
+    end
+  end
 
   private
 
