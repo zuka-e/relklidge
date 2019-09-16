d296c5ea2331c816bf12ae1f2b2f3777fa86bc9e
zuka-e <nq.iizuka@gmail.com>
2019-09-16 18:12:53 +0000

    Add admin_user-index table

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   has_many :post_tags
   has_many :posts, through: :post_tags
+  has_many :favorite_tags
+  has_many :users, through: :favorite_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   attachment :image
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorite_tags, dependent: :destroy
+  has_many :tags, through: :favorite_tags
 end
