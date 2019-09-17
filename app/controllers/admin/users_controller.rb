7a2ab4bb6a31090de1f6c10584da34c92b05826c
zuka-e <nq.iizuka@gmail.com>
2019-09-17 07:57:07 +0000

    Change display content

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,14 +3,6 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.page(params[:page])
     @users = @users.where(["name LIKE ?", "%#{params[:name]}%"]) if params[:name].present?
-    # @items = @items.where(["artist_id = ?","#{params[:artist]}"]) if params[:artist].present?
-    # @items = @items.where(["genre_id = ?", "#{params[:genre]}"]) if params[:genre].present?
-    # @items = @items.where(["sales_status = ?", "#{params[:sales_status]}"]) if params[:sales_status].present?
-    # if params[:key].present?
-    #   @items ||= @items.reorder("#{params[:key]} #{params[:direction]}")
-    #   @sort = sorted( @items, params[:key],params[:direction] )
-    #   @items = Kaminari.paginate_array(@sort).page(params[:page]).per(25)
-    # end
   end
 
   def show
