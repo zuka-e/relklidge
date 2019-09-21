182d0d65c697929ed84fd43a15b072be70061757
zuka-e <nq.iizuka@gmail.com>
2019-09-21 11:27:19 +0000

    Add before_action validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
 
   include SessionsHelper
+  before_action :admin_log_in_request
+
+  private
+
+    def admin_log_in_request
+      if request.fullpath.match?(/^\/admin\/*/) && !request.fullpath.match?(/^\/admin\/login/) && !admin_log_in?
+        session[:forwarding_url] = request.original_url # ログイン時に使用
+        flash[:danger] = "ログインが必要です"
+        redirect_to admin_login_url
+      end
+    end
 
 end
