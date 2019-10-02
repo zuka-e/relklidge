9bdeea83f8a609b511e5478c3f3598006e85b236
zuka-e <nq.iizuka@gmail.com>
2019-10-02 10:15:07 +0000

    Revise env-var for refile

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   require "refile/s3"
 
   aws = {
-    access_key_id: ENV['S3_ACCESS_KEY'],
-    secret_access_key: ENV['S3_SECRET_KEY'],
-    region: ENV['S3_REGION'],
-    bucket: ENV['S3_BUCKET'],
+    access_key_id: Rails.application.credentials.s3[:access_key],
+    secret_access_key: Rails.application.credentials.s3[:secret_access_key],
+    region:  Rails.application.credentials.s3[:region],
+    bucket:  Rails.application.credentials.s3[:bucket],
   }
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
